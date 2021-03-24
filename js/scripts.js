c97e41434ff3b50c0679eba11c8f952301af03e5
Min Chang <minchangmhc@gmail.com>
2021-03-24 13:59:41 -0700

    Added user interface for subtract
    Co-authored-by: Vanessa Su <vnessa.su@gmail.com>
    co-authored-by: Min Chang <minchangmhc@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,14 @@ $(document).ready(function () {
     const number1 = parseInt($("#add1").val());
     const number2 = parseInt($("#add2").val());
     const result = add(number1, number2);
-    $("#output").text(result);
-  })
+    $("#output-add").text(result);
+  });
 
-})
+  $("form#subtract").submit(function (event) {
+    event.preventDefault();
+    const number1 = parseInt($("#sub1").val());
+    const number2 = parseInt($("#sub2").val());
+    const result = subtract(number1, number2);
+    $("#output-subtract").text(result);
+  })
+});
